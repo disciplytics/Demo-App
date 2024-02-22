5e0c9cc27f1ebc9af0d0bbc0b99cd54f027c3339
disciplytics <122502050+disciplytics@users.noreply.github.com>
2024-02-22 16:44:25 -0500

    Add files via upload

---
+++ Hello.py
@@ -0,0 +1,25 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="Hello",
+    page_icon="👋",
+)
+
+st.write("# Disciplytics Demo App")
+st.sidebar.success("Select a demo above.")
+
+st.markdown(
+        """
+        Disciplytics provides analytics as a service to churches. 
+        
+        This app is a taste of what we can offer to your church!
+
+        **👈 Select a demo from the dropdown on the left** to see some examples
+        of what Disciplytics can do!
+
+        ### Want to learn more?
+
+        - Check out [Disciplytics](https://disciplytics.com)!
+
+    """
+    )
