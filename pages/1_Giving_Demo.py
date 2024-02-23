faba8a841243744555a53bf803a1f87359b724fc
disciplytics <122502050+disciplytics@users.noreply.github.com>
2024-02-23 16:34:27 -0500

    Update 1_Giving_Demo.py

--- pages/1_Giving_Demo.py
+++ pages/1_Giving_Demo.py
@@ -74,6 +74,8 @@ def clean_outliers(x):
 
 report_data['Donations'] = report_data['Donations'].apply(clean_outliers)
 
+report_data.loc[report_data['Date'].dt.isocalendar().week == 52, 'Donations'] = 50
+
 dynamic_filters = DynamicFilters(report_data, filters=['Year', 'Primary Campus', 'Age Group', 'Membership'])
 
 dynamic_filters.display_filters(location='columns', num_columns=4, gap='small')
