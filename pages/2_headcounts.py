638a0557a246c6f1f677627a0a196241beb2c7a0
disciplytics <122502050+disciplytics@users.noreply.github.com>
2024-06-21 13:18:15 -0400

    Update 2_headcounts.py

--- pages/2_headcounts.py
+++ pages/2_headcounts.py
@@ -1,24 +1,9 @@
 # import packages
-#from snowflake.snowpark.context import get_active_session
 import plotly.express as px
 import streamlit as st
 import pandas as pd
 import numpy as np
 from datetime import timedelta
-#from streamlit_dynamic_filters import DynamicFilters
-
-#st.set_page_config(page_title="Headcount Analytics",layout="wide")
-#st.title('Headcount Analytics')
-
-
-# Get current session
-#session = get_active_session()
-
-# Load ANALYICAL_ATTENDANCE
-#headcount_data = session.table(f'{st.secrets["database"]}.{st.secrets["schema"]}.HEADCOUNTS_DATA').to_pandas()
-#headcount_data = session.table(f'ANALYICAL_ATTENDANCE').to_pandas().reset_index(drop=True)
-#dynamic_filters = DynamicFilters(headcount_data, filters=['Event Name', 'Event Frequency', 'Attendance Time', 'Attendance Year', 'Event Day of Week'])
-#dynamic_filters.display_filters(location='columns', num_columns=5, gap='small')
 
 st.title('Headcount Report Demo')
 st.subheader("A Weekly Report For Attendance")
@@ -29,20 +14,20 @@ st.write(' ')
 def load_data():
     series_length = 3 * 52
     
-    event_names = ["Sunday Morning Adults", "Sunday Morning Kids", "Online Viewers"]
-    event_frequencies = ["Weekly"]
+    event_names = ["Campus 1 - Sunday Morning Service", "Campus 2 - Sunday Morning Service", "Campus 3 - Sunday Morning Service"]
+    attendance_types = ["Attendance", "Volunteer", "Youth"]
     start_at_times = ["10:00", "11:30"]
     
     
     report_data = pd.DataFrame()
     
     for i in event_names:
-        if i == 'Sunday Morning Adults':
+        if i == "Campus 1 - Sunday Morning Service":
             weight_i = 0.6
-        elif i == 'Sunday Morning Kids':
-            weight_i = 0.2
-        elif i == 'Online Viewers':
-            weight_i = 0.2
+        elif i == "Campus 2 - Sunday Morning Service":
+            weight_i = 0.3
+        elif i == "Campus 3 - Sunday Morning Service":
+            weight_i = 0.1
         for k in start_at_times:
             if k == '10:00':
                 weight_k = 0.4
@@ -61,10 +46,6 @@ def load_data():
                 })
                 
             data['Total Count'] = (data['Total Count'] * data['Weight']).astype(int)
-
-            data['Regular Count'] = (data['Total Count'] * 0.7).astype(int)
-            data['Guest Count'] = (data['Total Count'] * 0.2).astype(int)
-            data['Volunteer Count'] = (data['Total Count'] * 0.1).astype(int)
     
             report_data = pd.concat([report_data, data])
 
@@ -75,7 +56,7 @@ headcount_data = load_data()
 
 today = pd.to_datetime(pd.Timestamp.now().floor('d'), format="ISO8601", utc = True)
 
-for i in ['Total Count', 'Regular Count', 'Guest Count', 'Volunteer Count']:
+for i in ['Total Count']:
     headcount_data[i] = pd.to_numeric(headcount_data[i], downcast="integer")
 
 di = {'0': 'Sunday', '1': 'Monday', '2': 'Tuesday', '3': 'Wednesday', '4': 'Thursday', '5': 'Friday', '6': 'Saturday'}
@@ -149,60 +130,7 @@ except:
         pytd_total_counts = 0
         yoy = 0
         
-# get last sunday metrics
-last_week_sunday_attendance = headcount_data[(headcount_data['Event Date']  == headcount_data['Event Date'].max())]['Total Count'].sum()
 
-last_week_sunday_volunteer = headcount_data[(headcount_data['Event Date']  == headcount_data['Event Date'].max())]['Volunteer Count'].sum()
-
-last_week_sunday_regular = headcount_data[(headcount_data['Event Date']  == headcount_data['Event Date'].max())]['Regular Count'].sum()
-
-last_week_sunday_guest = headcount_data[(headcount_data['Event Date']  == headcount_data['Event Date'].max() )]['Guest Count'].sum()
-    
-    
-last_week_sunday_attendance_delta = int(headcount_data[(headcount_data['Event Date']  == two_weeks)]['Total Count'].sum())
-last_week_sunday_volunteer_delta = int(headcount_data[(headcount_data['Event Date']  == two_weeks)]['Volunteer Count'].sum())
-last_week_sunday_regular_delta = int(headcount_data[(headcount_data['Event Date']  == two_weeks)]['Regular Count'].sum())
-last_week_sunday_guest_delta = int(headcount_data[(headcount_data['Event Date']  == two_weeks)]['Guest Count'].sum())
-
-
-with col1:
-    try:
-        st.metric(label="Last Event Total Total", 
-             value=f"{format(int(round(last_week_sunday_attendance,0)), ',d')}",
-             delta_color = 'normal',
-             delta = f"{last_week_sunday_attendance - last_week_sunday_attendance_delta} From Prior Week")
-    except:
-        st.metric(label="Last Event Total Total", 
-             value=0)
-
-with col2:
-    try:
-        st.metric(label="Last Event Regular Total", 
-             value=f"{format(int(round(last_week_sunday_regular,0)), ',d')}",
-             delta_color = 'normal',
-             delta = f"{last_week_sunday_regular -last_week_sunday_regular_delta} From Prior Week")
-    except:
-        st.metric(label="Last Event Regular Total", 
-             value=0)
-with col3:
-    try:
-        st.metric(label="Last Event Guest Total", 
-             value=f"{format(int(round(last_week_sunday_guest,0)), ',d')}",
-             delta_color = 'normal',
-             delta = f"{last_week_sunday_guest -last_week_sunday_guest_delta} From Prior Week")
-    except:
-        st.metric(label="Last Event Guest Total", 
-             value=0)
-with col4:
-    try:
-        st.metric(label="Last Event Volunteer Total", 
-             value=f"{format(int(round(last_week_sunday_volunteer,0)), ',d')}",
-             delta_color = 'normal',
-             delta = f"{last_week_sunday_volunteer -last_week_sunday_volunteer_delta} From Prior Week")
-    except:
-        st.metric(label="Last Event Volunteer Total", 
-             value=0)
-        
         
 trend_df = df_selection.groupby(['Event Date', 'Event Year', 'Event Name', 'Event Time', 'Event Day of Week', 'Service'])['Total Count'].sum().reset_index()#.sort_values(ascending=True).reset_index()
 wow_df = df_selection.groupby(['Event Week', 'Event Year'])['Total Count'].sum().sort_values(ascending=True).reset_index()
@@ -224,52 +152,10 @@ wow_fig = px.bar(
 ).update_layout(yaxis_title=None, xaxis_title=None)
 
 
-wow_v_df = df_selection.drop(columns = ['Regular Count', 'Guest Count','Total Count'])
 
-wow_v_df = wow_v_df.groupby(['Event Year', 'Event Week'])['Volunteer Count'].sum().reset_index()
-
-wow_v_fig = px.bar(
-    wow_v_df,
-    x="Event Week",
-    y="Volunteer Count",
-    color = 'Event Year',
-    title = 'Volunteer Weekly Trends',
-    #render_mode='svg'
-).update_layout(yaxis_title=None, xaxis_title=None)
-
-
-wow_r_df = df_selection.drop(columns = ['Volunteer Count', 'Guest Count','Total Count'])
-
-wow_r_df = wow_r_df.groupby(['Event Year', 'Event Week'])['Regular Count'].sum().reset_index()
-
-wow_r_fig = px.bar(
-    wow_r_df,
-    x="Event Week",
-    y="Regular Count",
-    color = 'Event Year',
-    title = 'Regular Weekly Trends',
-    #render_mode='svg'
-).update_layout(yaxis_title=None, xaxis_title=None)
-
-wow_g_df = df_selection.drop(columns = ['Volunteer Count', 'Regular Count','Total Count'])
-
-wow_g_df = wow_g_df.groupby(['Event Year', 'Event Week'])['Guest Count'].sum().reset_index()
-
-wow_g_fig = px.bar(
-    wow_g_df,
-    x="Event Week",
-    y="Guest Count",
-    color = 'Event Year',
-    title = 'Regular Weekly Trends',
-    #render_mode='svg'
-).update_layout(yaxis_title=None, xaxis_title=None)
 
 table = df_selection.pivot_table('Total Count', ['Event Date'], 'Service').reset_index().sort_values(by = ['Event Date'], ascending=False)
 table['Event Date'] = pd.to_datetime(table['Event Date'], format='%Y%m%d', errors='coerce').dt.strftime('%Y-%m-%d')
-tab1, tab2, tab3, tab4, tab5, tab6 = st.tabs(["Headcount Trend", 'Headcounts Weekly Trend', 'Volunteer Weekly Trend', 'Regular Weekly Trend', 'Guest Weekly Trend', 'Headcount Table'])
+tab1, tab2 = st.tabs(["Headcount Trend", 'Headcount Table'])
 tab1.plotly_chart(trend_fig, theme="streamlit", use_container_width=True)
-tab2.plotly_chart(wow_fig, theme="streamlit", use_container_width=True)
-tab3.plotly_chart(wow_v_fig, theme="streamlit", use_container_width=True)
-tab4.plotly_chart(wow_r_fig, theme="streamlit", use_container_width=True)
-tab5.plotly_chart(wow_g_fig, theme="streamlit", use_container_width=True)
-tab6.dataframe(table.fillna(0))
+tab2.dataframe(table.fillna(0))
